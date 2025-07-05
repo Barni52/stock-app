1a77a0470d860aa1df290a55171af3e5761f6039
Barni52 <szabo.barnabas.mihaly@gmail.com>
2025-07-05 15:48:42 +0200

    Add withdraw and upload money options

--- backend/stock/src/main/java/org/example/stock/controller/MainController.java
+++ backend/stock/src/main/java/org/example/stock/controller/MainController.java
@@ -1,6 +1,7 @@
 package org.example.stock.controller;
 
 import org.example.stock.model.Stock;
+import org.example.stock.model.StockUser;
 import org.example.stock.repository.StockRepository;
 import org.example.stock.repository.StockUserRepository;
 import org.example.stock.service.StockParsingService;
@@ -10,29 +11,29 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
-public class HomeController {
+public class MainController {
 
     private final StockUserRepository userRepository;
     private final TwelveDataService twelveDataService;
     private final StockRepository stockRepository;
     private final StockParsingService stockParsingService;
+    private final StockUserRepository stockUserRepository;
 
-    public HomeController(StockUserRepository userRepository,
+    public MainController(StockUserRepository userRepository,
                           TwelveDataService twelveDataService,
                           StockRepository stockRepository,
-                          StockParsingService stockParsingService) {
+                          StockParsingService stockParsingService, StockUserRepository stockUserRepository) {
         this.userRepository = userRepository;
         this.twelveDataService = twelveDataService;
         this.stockRepository = stockRepository;
         this.stockParsingService = stockParsingService;
+        this.stockUserRepository = stockUserRepository;
     }
 
     @RequestMapping("/")
@@ -60,5 +61,36 @@ public class HomeController {
         stockRepository.save(stock);
 
         return ResponseEntity.ok(List.of(data));
+    }
+
+    @PreAuthorize("#username == authentication.name")
+    @PutMapping("/balance/upload/{username}")
+    public ResponseEntity<Void> uploadBalance(@RequestParam Double value, @PathVariable String username){
+        Optional<StockUser> stockUserOptional = stockUserRepository.findByUsername(username);
+        if(stockUserOptional.isPresent()){
+            StockUser stockUser = stockUserOptional.get();
+            stockUser.setBalance(stockUser.getBalance() + value);
+            stockUserRepository.save(stockUser);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.status(400).build();
+    }
+
+    @PreAuthorize("#username == authentication.name")
+    @PutMapping("/balance/withdraw/{username}")
+    public ResponseEntity<Void> withdrawBalance(@RequestParam Double value, @PathVariable String username){
+        Optional<StockUser> stockUserOptional = stockUserRepository.findByUsername(username);
+
+        if(stockUserOptional.isPresent()){
+            StockUser stockUser = stockUserOptional.get();
+            if(value <= stockUser.getBalance()){
+                stockUser.setBalance(stockUser.getBalance() - value);
+                stockUserRepository.save(stockUser);
+                return ResponseEntity.ok().build();
+            } else{
+                return ResponseEntity.status(401).build();
+            }
+        }
+        return ResponseEntity.status(400).build();
     }
 }
